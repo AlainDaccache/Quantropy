5fe78a6daf4f223393d01d06038e88ffb2f43313
Alain Daccache <alain.daccache@mail.mcgill.ca>
2021-03-01 18:05:00 -0500

    travis/docker/pypi integration

--- matilda/__init__.py
+++ matilda/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_login import LoginManager
-from .database.object_model import User
-from .database.db_crud import get_atlas_db_url, connect_to_mongo_engine
-from .api_routes.auth import auth as auth_blueprint
-from .api_routes.main import main as main_blueprint
+
+from matilda.database.object_model import User
+from matilda.database.db_crud import get_atlas_db_url, connect_to_mongo_engine
+from matilda.api_routes.auth import auth as auth_blueprint
+from matilda.api_routes.main import main as main_blueprint
 
 atlas_url = get_atlas_db_url(username='AlainDaccache', password='qwerty98', dbname='matilda-db')
 db = connect_to_mongo_engine(atlas_url)
@@ -26,3 +27,10 @@ def load_user(user_id):
     return User.objects(_id=user_id).first()
 
 
+"""
+You can run the flask app in terminal with the commands 'set FLASK_APP=matilda', then 'py -m flask run'
+When it says 'Running on http://0.0.0.0:5000/', it means it is accepting connections on any network adapter,
+not a specific one. Use 127.0.0.1 i.e. 'http://localhost:5000/' to actually connect to a server running on your machine.
+"""
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
