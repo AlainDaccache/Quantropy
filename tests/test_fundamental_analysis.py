3ebd452e2c2cf76e049a102ec2931479b5b5807f
Alain Daccache <alain.daccache@mail.mcgill.ca>
2021-03-14 21:38:55 -0400

    database integration, improving backtester design

--- tests/test_fundamental_analysis.py
+++ tests/test_fundamental_analysis.py
@@ -9,7 +9,7 @@ class TestFinancialStatementsEntries(unittest.TestCase):
         self.client = connect_to_mongo_engine(atlas_url)
 
     def test_current_assets(self):
-        asserts = [76219000000, 89378000000, 304441000000, 0]
+        asserts = [76219000000, 89378000000, 304876000000, 0]
         for i, period in enumerate(['Q', 'FY', 'TTM', 'YTD']):
             with self.subTest(i=i):
                 self.assertEqual(total_current_assets(stock='AAPL', date=datetime(2016, 1, 1), period=period),
